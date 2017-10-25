d6c175bb48156e9fd5b6fe0d684fe25e5a3a49d5
081894brayan <brayanestive18@gmail.com>
2017-10-25 15:26:09 -0500

    V2

---
+++ app/src/main/java/com/brayadiaz/agendafirebase/AgendaSQLite.java
@@ -0,0 +1,13 @@
+package com.brayadiaz.agendafirebase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AgendaSQLite extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
