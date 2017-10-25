53a0421882b2e471301d422a7de3647155f787dd
081894brayan <brayanestive18@gmail.com>
2017-10-25 16:30:53 -0500

    V2

--- app/src/main/java/com/brayadiaz/agendafirebase/AgendaSQLite.java
+++ app/src/main/java/com/brayadiaz/agendafirebase/AgendaSQLite.java
@@ -1,11 +1,15 @@
 package com.brayadiaz.agendafirebase;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.design.widget.NavigationView;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.Toast;
 
 public class AgendaSQLite extends BaseActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -13,10 +17,15 @@ public class AgendaSQLite extends BaseActivity
     EditText eID, eName, eEmail, ePhone;
     Button bCreate, bUpdate, bRead, bDelete;
 
+    ContactosSQLiteHelper contactosSQLiteHelper;
+    SQLiteDatabase dbContactos;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        FrameLayout contentFrameLayout = (FrameLayout) findViewById(R.id.frame);
+        getLayoutInflater().inflate(R.layout.activity_main, contentFrameLayout);
 
         eID = (EditText) findViewById(R.id.eID);
         eName = (EditText) findViewById(R.id.eName);
@@ -26,6 +35,9 @@ public class AgendaSQLite extends BaseActivity
         bUpdate = (Button) findViewById(R.id.bUpdate);
         bRead = (Button) findViewById(R.id.bRead);
         bDelete = (Button) findViewById(R.id.bDelete);
+
+        contactosSQLiteHelper = new ContactosSQLiteHelper(this, "AgendaDB", null, 1);
+        dbContactos = contactosSQLiteHelper.getWritableDatabase();
 
     }
 
@@ -40,11 +52,27 @@ public class AgendaSQLite extends BaseActivity
         switch (id){
             case R.id.bCreate:
 
+                ContentValues data = new ContentValues();
+                data.put("name",name);
+                data.put("phone",phone);
+                data.put("email",email);
+
+                dbContactos.insert("contactos", null,data);
+                clean();
+
                 break;
             case R.id.bUpdate:
 
                 break;
             case R.id.bRead:
+
+                Cursor c = dbContactos.rawQuery("SELECT * FROM contactos WHERE name='"+name+"'",null);
+                if (c.moveToFirst()){
+                    ePhone.setText(c.getString(2));
+                    eEmail.setText(c.getString(3));
+                } else {
+                    Toast.makeText(this, "No Existe", Toast.LENGTH_SHORT).show();
+                }
 
                 break;
             case R.id.bDelete:
